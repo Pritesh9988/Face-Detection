772ad98c0bd7d042a5e62cd4b320e9c37782fbfc
Pritesh <priteshlonkar007@gmail.com>
2022-03-07 21:52:08 +0530

    final commit

--- model.py
+++ model.py
@@ -5,12 +5,6 @@ import cv2
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
-
-# data/haarcascades/haarcascade_frontalface_default.xml
-# Trained XML classifiers describes some features of some
-# object we want to detect a cascade function is trained
-# from a lot of positive(faces) and negative(non-faces)
-# images.
 try:
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 except Exception:
@@ -19,52 +13,28 @@ except Exception:
 # Trained XML file for detecting eyes
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
   
-# capture frames from a camera
-cap = cv2.VideoCapture(0)
-  
-# loop runs if capturing has been initialized.
+
 class VideoTransformer(VideoTransformerBase):
-    while 1: 
-    
-        # reads frames from a camera
-        ret, img = cap.read() 
-    
-        # convert to gray scale of each frames
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-        # Detects faces of different sizes in the input image
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    
-        for (x,y,w,h) in faces:
-            # To draw a rectangle in a face 
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-    
-            # Detects eyes of different sizes in the input image
-            eyes = eye_cascade.detectMultiScale(roi_gray) 
-    
-            #To draw a rectangle in eyes
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
-    
-        # Display an image in a window
-        cv2.imshow('img',img)
-    
-        # Wait for Esc key to stop
-        # k = cv2.waitKey(30) & 0xff
-        # if k == 27:
-            # break
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            
+        return img
   
-# Close the window
-# cap.release()
-# De-allocate any associated memory usage
-# cv2.destroyAllWindows() 
 def main():
     # Face Analysis Application #
     st.title("Real Time Face Emotion Detection Application")
-    activiteis = ["Home", "Webcam Face Detection", "About"]
-    choice = st.sidebar.selectbox("Select Activity", activiteis)
+    activities = ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
     st.sidebar.markdown(
         """ Developed by pritesh lonkar    
             Email : priteshlonkar007@gmail.com  
@@ -79,7 +49,6 @@ def main():
         st.write("""
                  The application has two functionalities.
                  1. Real time face detection using web cam feed.
-                 2. Real time face emotion recognization.
                  """)
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
@@ -97,7 +66,7 @@ def main():
 
         html_temp4 = """
                              		<div style="background-color:#98AFC7;padding:10px">
-                             		<h4 style="color:white;text-align:center;">This Application is developed by Mohammad Juned Khan using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at Mohammad.juned.z.khan@gmail.com. </h4>
+                             		<h4 style="color:white;text-align:center;">This Application is developed by Pritesh lonkar using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail at Mohammad.juned.z.khan@gmail.com. </h4>
                              		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
                              		</div>
                              		<br></br>
